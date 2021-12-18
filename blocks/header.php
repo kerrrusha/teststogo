096b159159478343a35bbd296f65aeba8477a288
kerrrusha <brcyrill2@gmail.com>
2021-12-18 05:19:52 +0200

    js header fix

--- blocks/header.php
+++ blocks/header.php
@@ -1,9 +1,11 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom mb-3 px-3 rounded">
-        <a href="../index.php"><img src="../images/logo.png" class='img w-50' style="max-width: 500px; min-width: 300px;"></a>
+        <a href="../index.php" class="text-center">
+        	<img src="../images/logo.png" class="img" id='logo' style="max-width: 350px; min-width: 100px; width:100%;">
+        </a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" id="toggle_button" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -30,8 +32,24 @@
 	            </div>
 	        </li> -->
           </ul>
-          <form class="form-inline mx-auto">
+          <form class="form-inline">
             <input class="form-control" type="text" placeholder="Пошук . . ." aria-label="Search">
           </form>
         </div>
 </nav>
+
+<!-- немного логики отображения -->
+<script type="text/javascript">
+	//согласование размеров логотипа с текущей шириной экрана
+	var scale = function() {
+	  	var logo = document.getElementById('logo');
+	  	var toggle_button = document.getElementById('toggle_button');
+		var window_width = document.documentElement.clientWidth;
+
+		//отступы в пикселях
+		var margins = 100;
+		logo.style.width = (window_width - margins - toggle_button.offsetWidth).toString() + 'px';
+	}
+	window.addEventListener('resize', scale);
+	document.addEventListener('DOMContentLoaded', scale);
+</script>
